25bd7b98fcbf8f5f43bb3804a2d02dc60fcd82e9
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-02 11:12:09 +0100

    prototype improvement
    * make button clickable only if name textview is not empty
    * fix bouncing ball animation behaviour

--- app/src/main/java/com/multipong/MainActivity.java
+++ app/src/main/java/com/multipong/MainActivity.java
@@ -1,15 +1,22 @@
 package com.multipong;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.BounceInterpolator;
 import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +26,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        setContentView(R.layout.activity_main);
+
+        final TextView mNameTextView = (TextView) findViewById(R.id.player_name);
+        final Button mPlayButton = (Button) findViewById(R.id.play_button);
+
         bouncingBall();
+        mNameTextView.addTextChangedListener(new InputTextWatcher(mPlayButton));
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Play button clicked!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 
     private void bouncingBall() {
@@ -46,17 +69,28 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                Log.i("bounce",
-                        "Ending button dropdown animation. Clearing animation and setting layout");
-                bounceBallImage.clearAnimation();
-                final int left = bounceBallImage.getLeft();
-                final int top = bounceBallImage.getTop()+110;
-                final int right = bounceBallImage.getRight();
-                final int bottom = bounceBallImage.getBottom()+110;
-                bounceBallImage.layout(left, top, right, bottom);
-
             }
         });
         bounceBallImage.startAnimation(transAnim);
+    }
+
+    private class InputTextWatcher implements TextWatcher {
+        private Button mButton;
+
+        InputTextWatcher(Button button) { mButton = button; }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int st, int c, int a) { }
+
+        @Override
+        public void onTextChanged(CharSequence s, int st, int bef, int c) { }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if(s.length() == 0)
+                mButton.setClickable(false);
+            else
+                mButton.setClickable(true);
+        }
     }
 }

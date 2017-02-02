d3447fe0b06201d83904e1ba02c84deb80f3f118
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-02 12:23:20 +0100

    Change input control on Play

--- app/src/main/java/com/multipong/MainActivity.java
+++ app/src/main/java/com/multipong/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.view.animation.BounceInterpolator;
 import android.view.animation.TranslateAnimation;
@@ -30,10 +28,16 @@ public class MainActivity extends AppCompatActivity {
         final TextView mNameTextView = (TextView) findViewById(R.id.player_name_tv);
         final Button mPlayButton = (Button) findViewById(R.id.play_button);
 
-        mNameTextView.addTextChangedListener(new InputTextWatcher(mPlayButton));
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(mNameTextView.getText().length() == 0) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please insert your name";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast.makeText(context, text, duration).show();
+                    return;
+                }
                 Context context = getApplicationContext();
                 CharSequence text = "Play button clicked!";
                 int duration = Toast.LENGTH_SHORT;
@@ -56,26 +60,6 @@ public class MainActivity extends AppCompatActivity {
         transAnim.setFillAfter(true);
         transAnim.setInterpolator(new BounceInterpolator());
         bounceBallImage.startAnimation(transAnim);
-    }
-
-    private class InputTextWatcher implements TextWatcher {
-        private Button mButton;
-
-        InputTextWatcher(Button button) { mButton = button; }
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int st, int c, int a) { }
-
-        @Override
-        public void onTextChanged(CharSequence s, int st, int bef, int c) { }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            if(s.length() == 0)
-                mButton.setClickable(false);
-            else
-                mButton.setClickable(true);
-        }
     }
 
     private class CreditsListener implements View.OnClickListener {

3a80f4a7f9071331fa93b8ee17898243cf49268b
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-08 13:56:01 +0100

    Add draft of PeerExplorer

--- app/src/main/java/com/multipong/net/PeerExplorer.java
+++ app/src/main/java/com/multipong/net/PeerExplorer.java
@@ -1,13 +1,29 @@
 package com.multipong.net;
 
 import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+
+import java.util.ArrayList;
 
 /**
  * PeerExplorer discovers new peers.
  */
 public class PeerExplorer {
 
-    private Integer getIdForNode(WifiP2pDevice device) {
+    WifiP2pManager manager;
+    Channel channel;
+
+    public PeerExplorer(WifiP2pManager manager1, Channel channel1) {
+        manager = manager1;
+        channel = channel1;
+    }
+
+    public ArrayList<WifiP2pDevice> discover() {
+        return null;
+    }
+
+    private static Integer getIdForNode(WifiP2pDevice device) {
         String name = device.deviceName;
         String mac = device.deviceAddress;
         int nameHash = name.hashCode();

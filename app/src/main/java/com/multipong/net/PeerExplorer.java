e3172df13bc923161d888fd1f55a879fd1605b8f
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-08 11:49:52 +0100

    Add hash generation for WifiP2pDevice objects

--- app/src/main/java/com/multipong/net/PeerExplorer.java
+++ app/src/main/java/com/multipong/net/PeerExplorer.java
@@ -1,7 +1,17 @@
 package com.multipong.net;
 
+import android.net.wifi.p2p.WifiP2pDevice;
+
 /**
  * PeerExplorer discovers new peers.
  */
 public class PeerExplorer {
+
+    private Integer getIdForNode(WifiP2pDevice device) {
+        String name = device.deviceName;
+        String mac = device.deviceAddress;
+        int nameHash = name.hashCode();
+        int macHash = mac.hashCode();
+        return nameHash * 31 + macHash;
+    }
 }

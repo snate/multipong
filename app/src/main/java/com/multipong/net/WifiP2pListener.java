f62b939b6867a14848c8a0395695b05e0cf28b4f
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-08 15:33:57 +0100

    Notify activity even if no peers have been found after discovery

--- app/src/main/java/com/multipong/net/WifiP2pListener.java
+++ app/src/main/java/com/multipong/net/WifiP2pListener.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
+import android.util.Log;
 
 public class WifiP2pListener extends BroadcastReceiver {
 
@@ -17,6 +18,17 @@ public class WifiP2pListener extends BroadcastReceiver {
         mManager = manager;
         mChannel = channel;
         mListener = listener;
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                mManager.requestPeers(mChannel, mListener);
+            }
+
+            @Override
+            public void onFailure(int reason) {
+
+            }
+        });
     }
 
     @Override
@@ -24,6 +36,12 @@ public class WifiP2pListener extends BroadcastReceiver {
         String action = intent.getAction();
 
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                // Wifi P2P is enabled
+            } else {
+                // this check is for debug purposes
+            }
             // Check to see if Wi-Fi is enabled and notify appropriate activity
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
             mManager.requestPeers(mChannel, mListener);

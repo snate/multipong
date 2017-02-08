8a26ca42de41433d742f329f5b2a55890048a8d8
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-08 14:03:03 +0100

    Remove hash computation for device from PeerExplorer

--- app/src/main/java/com/multipong/net/PeerExplorer.java
+++ app/src/main/java/com/multipong/net/PeerExplorer.java
@@ -22,12 +22,4 @@ public class PeerExplorer {
     public ArrayList<WifiP2pDevice> discover() {
         return null;
     }
-
-    private static Integer getIdForNode(WifiP2pDevice device) {
-        String name = device.deviceName;
-        String mac = device.deviceAddress;
-        int nameHash = name.hashCode();
-        int macHash = mac.hashCode();
-        return nameHash * 31 + macHash;
-    }
 }

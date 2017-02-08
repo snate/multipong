38ff46d4652d73859f36621d9121c542b4b13fc4
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-08 14:58:23 +0100

    Add draft for peer discovery

--- app/src/main/java/com/multipong/activity/MultiplayerGameFormationActivity.java
+++ app/src/main/java/com/multipong/activity/MultiplayerGameFormationActivity.java
@@ -1,17 +1,27 @@
 package com.multipong.activity;
 
 import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.multipong.R;
+import com.multipong.net.PeerExplorer;
+import com.multipong.net.WifiP2pListener;
+
+import java.util.List;
 
 public class MultiplayerGameFormationActivity extends AppCompatActivity {
 
-    WifiP2pManager mManager;
-    Channel mChannel;
+    private WifiP2pManager mManager;
+    private Channel mChannel;
+    private PeerExplorer mExplorer;
+    private WifiP2pListener mWifiP2pListener;
+    private IntentFilter mIntentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,38 @@ public class MultiplayerGameFormationActivity extends AppCompatActivity {
 
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mManager.initialize(this, getMainLooper(), null);
+        mExplorer = new PeerExplorer(this);
+        mWifiP2pListener = new WifiP2pListener(mManager, mChannel, mExplorer);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mWifiP2pListener, mIntentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mWifiP2pListener);
+    }
+
+    public void receiveList(List<WifiP2pDevice> list) {
+        showShortToast("All good");
+        /*if (list != null)
+            showShortToast("YAHOO");*/
+    }
+
+    private void showShortToast(String toastText) {
+        Context context = getApplicationContext();
+        CharSequence text = toastText;
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
     }
 }
--- app/src/main/java/com/multipong/net/PeerExplorer.java
+++ app/src/main/java/com/multipong/net/PeerExplorer.java
@@ -1,25 +1,47 @@
 package com.multipong.net;
 
 import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
-import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.util.Log;
+
+import com.multipong.activity.MultiplayerGameFormationActivity;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * PeerExplorer discovers new peers.
  */
-public class PeerExplorer {
+public class PeerExplorer implements WifiP2pManager.PeerListListener {
 
-    WifiP2pManager manager;
-    Channel channel;
+    MultiplayerGameFormationActivity mActivity;
+    private List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
 
-    public PeerExplorer(WifiP2pManager manager1, Channel channel1) {
-        manager = manager1;
-        channel = channel1;
+    public PeerExplorer(MultiplayerGameFormationActivity activity) {
+        mActivity = activity;
     }
 
-    public ArrayList<WifiP2pDevice> discover() {
-        return null;
+    @Override
+    public void onPeersAvailable(WifiP2pDeviceList peerList) {
+        List<WifiP2pDevice> refreshedPeers = (List<WifiP2pDevice>) peerList.getDeviceList();
+        mActivity.receiveList(peers);
+        if (!refreshedPeers.equals(peers)) {
+            peers.clear();
+            peers.addAll(refreshedPeers);
+
+            // If an AdapterView is backed by this data, notify it
+            // of the change.  For instance, if you have a ListView of
+            // available peers, trigger an update.
+            // ((WiFiPeerListAdapter) getListAdapter()).notifyDataSetChanged();
+
+            // Perform any other updates needed based on the new list of
+            // peers connected to the Wi-Fi P2P network.
+        }
+
+        if (peers.size() == 0) {
+            Log.d("PEER_EXPLORER", "No devices found");
+            return;
+        }
     }
 }

f7b214ee17fd3bbefcbc564da41d1220f2795798
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-08 14:03:17 +0100

    minor refactoring for NameResolutor

--- app/src/main/java/com/multipong/net/NameResolutor.java
+++ app/src/main/java/com/multipong/net/NameResolutor.java
@@ -17,38 +17,34 @@ public class NameResolutor {
     }
 
     public void addNode (WifiP2pDevice newNode) {
-        nodes.put(hashOf(newNode.deviceAddress+newNode.deviceName), newNode);
+        nodes.put(hashOf(newNode), newNode);
     }
 
     public WifiP2pDevice getNodeByHash (int hash) {
         return nodes.get(hash);
     }
 
-    public WifiP2pDevice getNodeByName (String deviceName) {
-        Iterator it = nodes.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            if ((((WifiP2pDevice)pair.getValue()).deviceName).equals(deviceName))
-                return ((WifiP2pDevice)pair.getValue());
-        }
+    public WifiP2pDevice getNodeByName (String name) {
+        for (WifiP2pDevice device : nodes.values())
+            if (device.deviceName.equals(name))
+                return device;
         return null;
     }
 
-    public WifiP2pDevice getNodeByAddress (String deviceAddress) {
-        Iterator it = nodes.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            if ((((WifiP2pDevice)pair.getValue()).deviceAddress).equals(deviceAddress))
-                return ((WifiP2pDevice)pair.getValue());
-        }
+    public WifiP2pDevice getNodeByAddress (String address) {
+        for (WifiP2pDevice device : nodes.values())
+            if (device.deviceAddress.equals(address))
+                return device;
         return null;
     }
 
-    private Integer hashOf(String string) {
-        int hash = 7;
-        for (int i = 0; i < string.length(); i++) {
-            hash = hash*31 + string.charAt(i);
-        }
-        return hash;
+
+
+    public static Integer hashOf(WifiP2pDevice device) {
+        String name = device.deviceName;
+        String mac = device.deviceAddress;
+        int nameHash = name.hashCode();
+        int macHash = mac.hashCode();
+        return nameHash * 31 + macHash;
     }
 }

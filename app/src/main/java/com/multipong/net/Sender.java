03c5822689aeab4128535d8ced3caa3e0d3abbe9
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-08 20:20:05 +0100

    Add non working implementation for communication between peers

--- app/src/main/java/com/multipong/net/Sender.java
+++ app/src/main/java/com/multipong/net/Sender.java
@@ -1,13 +1,55 @@
 package com.multipong.net;
 
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 /**
  * Sender has the task of sending packets to a given peer
  */
-public class Sender {
+public class Sender extends IntentService {
     private int id;
     private NameResolutor nameResolutor;
 
-    public void send(int node, Object msg){}
+    private static final int SOCKET_TIMEOUT = 5000;
+    public static final String ACTION_SEND_FILE = "com.multipong.net.SEND_FILE";
+    public static final String EXTRAS_ADDRESS = "sender_host";
+    public static final String EXTRAS_PORT = "sender_port";
+
+    public Sender(String name) {
+        super(name);
+    }
+
+    public Sender() {
+        super("SenderService");
+    }
+
+    public void send(int node, Object msg){
+
+    }
 
     public void sendToCoordinator(Object msg){}
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d("Sender", "Intent received");
+        Context context = getApplicationContext();
+        String host = "localhost";
+        int port = 8888;
+        if (!intent.getAction().equals(ACTION_SEND_FILE)) return;
+        String data = "Hello, world";
+        Socket socket = new Socket();
+        try {
+            socket.bind(null);
+            socket.connect(new InetSocketAddress(host, port), SOCKET_TIMEOUT);
+            Log.d("Sender", "Client socket - " + socket.isConnected());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/multipong/net/WifiP2pListener.java
+++ app/src/main/java/com/multipong/net/WifiP2pListener.java
@@ -3,21 +3,28 @@ package com.multipong.net;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
 import android.util.Log;
+
+import com.multipong.activity.MultiplayerGameFormationActivity;
 
 public class WifiP2pListener extends BroadcastReceiver {
 
     private WifiP2pManager mManager;
     private Channel mChannel;
     private PeerListListener mListener;
+    private MultiplayerGameFormationActivity mActivity;
 
-    public WifiP2pListener(WifiP2pManager manager, Channel channel, PeerListListener listener) {
+    public WifiP2pListener(WifiP2pManager manager, Channel channel,
+                           MultiplayerGameFormationActivity activity) {
         mManager = manager;
         mChannel = channel;
-        mListener = listener;
+        mListener = new PeerExplorer(activity);
+        mActivity = activity;
         mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
             @Override
             public void onSuccess() {
@@ -37,19 +44,41 @@ public class WifiP2pListener extends BroadcastReceiver {
 
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
             int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+
             if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
                 // Wifi P2P is enabled
             } else {
                 // this check is for debug purposes
             }
             // Check to see if Wi-Fi is enabled and notify appropriate activity
+
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
             mManager.requestPeers(mChannel, mListener);
-            // Call WifiP2pManager.requestPeers() to get a list of current peers
+
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
-            // Respond to new connection or disconnections
+            Log.d("WifiP2pListener", "Uh?");
+            if (mManager == null) { return; }
+            NetworkInfo networkInfo = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+            if (networkInfo.isConnected()) {
+                // Connection
+                mManager.requestConnectionInfo(mChannel, new MyConnectionListener());
+            } else {
+                // Disconnection
+
+            }
+
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
             // Respond to this device's wifi state changing
+        }
+    }
+
+    private class MyConnectionListener implements WifiP2pManager.ConnectionInfoListener {
+
+        @Override
+        public void onConnectionInfoAvailable(WifiP2pInfo info) {
+            Log.d("MyConnectionLister", "here");
+            Intent serviceIntent = new Intent(mActivity, Sender.class);
+            serviceIntent.setAction(Sender.ACTION_SEND_FILE);
         }
     }
 }

1bc2768a059416b0f20b4f6f58a4c8ee99bf7f16
mzanella <zanna0150@gmail.com>
2017-02-08 13:53:07 +0100

    Changed sender type of messages

--- app/src/main/java/com/multipong/net/Sender.java
+++ app/src/main/java/com/multipong/net/Sender.java
@@ -4,5 +4,10 @@ package com.multipong.net;
  * Sender has the task of sending packets to a given peer
  */
 public class Sender {
+    private int id;
+    private NameResolutor nameResolutor;
 
+    public void send(int node, Object msg){}
+
+    public void sendToCoordinator(Object msg){}
 }

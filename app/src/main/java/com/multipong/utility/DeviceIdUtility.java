b66f8dee924a0e225ab289ef1b5cefd430d08d14
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-10 11:38:14 +0100

    Narrow visibility of DeviceIdUtility's field

--- app/src/main/java/com/multipong/utility/DeviceIdUtility.java
+++ app/src/main/java/com/multipong/utility/DeviceIdUtility.java
@@ -2,7 +2,7 @@ package com.multipong.utility;
 
 public class DeviceIdUtility {
 
-    public static Integer id;
+    private static Integer id;
 
     public static Integer getId() {
         return id;

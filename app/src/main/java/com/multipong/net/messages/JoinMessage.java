581309fd54f48e5bd511d74b2fa985b3a66cd8c8
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-10 11:38:26 +0100

    Add ctor for JoinMessage

--- app/src/main/java/com/multipong/net/messages/JoinMessage.java
+++ app/src/main/java/com/multipong/net/messages/JoinMessage.java
@@ -1,8 +1,28 @@
 package com.multipong.net.messages;
 
 import com.multipong.model.formation.Participant;
+import com.multipong.utility.DeviceIdUtility;
+
+import org.json.JSONException;
+
+import java.util.Map;
 
 public class JoinMessage extends Message {
+
+    public static final String ID_FIELD = "newId";
+
+    // TODO: Precondition - here I assume that DeviceUtility.id has been set
+    //       when creating a participant-side JOIN message
+    public JoinMessage() {
+        super();
+        Integer myId = DeviceIdUtility.getId();
+        try {
+            object.put(ID_FIELD, myId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected String getMessageType() {
         return Participant.MessageType.JOIN;

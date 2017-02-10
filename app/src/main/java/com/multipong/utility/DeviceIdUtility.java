30e5e2a2228691b5e4afe5f96df364b8221e94b0
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-10 10:56:14 +0100

    Add DeviceIdUtility

---
+++ app/src/main/java/com/multipong/utility/DeviceIdUtility.java
@@ -0,0 +1,14 @@
+package com.multipong.utility;
+
+public class DeviceIdUtility {
+
+    public static Integer id;
+
+    public static Integer getId() {
+        return id;
+    }
+
+    public static void setId(Integer id) {
+        DeviceIdUtility.id = id;
+    }
+}

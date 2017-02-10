58e7503cbf36e390a5c07e5afbd5813bf380c273
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-10 11:19:42 +0100

    Implement discover response for Host

--- app/src/main/java/com/multipong/model/formation/Host.java
+++ app/src/main/java/com/multipong/model/formation/Host.java
@@ -1,13 +1,18 @@
 package com.multipong.model.formation;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
 import com.multipong.activity.MultiplayerGameFormationActivity;
 import com.multipong.model.Actor;
 import com.multipong.net.NameResolutor;
+import com.multipong.net.Sender;
+import com.multipong.net.Utils;
+import com.multipong.net.messages.AvailableMessage;
 import com.multipong.net.messages.DiscoverMessage;
+import com.multipong.net.messages.Message;
 import com.multipong.utility.DeviceIdUtility;
 
 import org.json.JSONObject;
@@ -39,15 +44,21 @@ public class Host implements Actor {
     @Override
     public synchronized void receive(String type, JSONObject message, InetAddress sender) {
         switch (type) {
-            case Participant.MessageType.DISCOVER: discover(message, sender);
+            case Participant.MessageType.DISCOVER: discover(sender);
             case Participant.MessageType.JOIN: break;
             case Participant.MessageType.CANCEL: break;
         }
     }
 
-    private void discover(JSONObject jsonObject, InetAddress sender) {
-        DiscoverMessage message = DiscoverMessage.createFromJson(jsonObject);
-        // TODO: Add implementation
+    private void discover(InetAddress sender) {
+        Intent sendResponse = new Intent(activity, Sender.class);
+        sendResponse.setAction(Sender.ACTION_SEND_MESSAGE);
+        sendResponse.putExtra(Sender.EXTRAS_ADDRESS, sender.toString());
+        sendResponse.putExtra(Sender.EXTRAS_PORT, Utils.PORT);
+        AvailableMessage response = new AvailableMessage();
+        response.addParticipants(participants);
+        sendResponse.putExtra(Sender.EXTRAS_CONTENT, response.getMsg().toString());
+        activity.startService(sendResponse);
     }
 
     public class MessageType {

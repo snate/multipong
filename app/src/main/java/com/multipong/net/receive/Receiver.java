e8cc6e5deda4742d5a586043d064ff129a7be5ab
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-11 18:25:41 +0100

    Move TCP receive in separate class

--- app/src/main/java/com/multipong/net/receive/Receiver.java
+++ app/src/main/java/com/multipong/net/receive/Receiver.java
@@ -9,18 +9,13 @@ import com.multipong.net.messages.Message;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
+import java.net.InetAddress;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Receiver implements Runnable {
+public abstract class Receiver implements Runnable {
 
     private MultiplayerGameFormationActivity activity;
-    private ServerSocket serverSocket;
     private ExecutorService executor;
 
     public Receiver(MultiplayerGameFormationActivity activity) {
@@ -29,50 +24,31 @@ public class Receiver implements Runnable {
 
     @Override
     public void run() {
+        Log.d("RECEIVER", "Started");
         int cores = Runtime.getRuntime().availableProcessors();
         executor = Executors.newFixedThreadPool(cores * 2);
-        Log.d("RECEIVER", "Started");
-        try {
-            serverSocket = new ServerSocket(Utils.PORT);
-            while(true) {
-                final Socket client = serverSocket.accept();
+    }
 
-                try {
-                    InputStream input = client.getInputStream();
-                    Scanner scanner = new Scanner(input).useDelimiter("\\A");
-                    String json = scanner.hasNext() ? scanner.next() : "";
-                    if (!Utils.isJsonObject(json)) continue;
-                    final JSONObject jsonObject = new JSONObject(json);
-                    if(!jsonObject.has(Message.APP_FIELD)) continue;
-                    String app = jsonObject.getString(Message.APP_FIELD);
-                    if(!app.equals(Message.APP_VALUE)) continue;
-                    String name = jsonObject.getString(Message.NAME_FIELD);
-                    final String type = jsonObject.getString(Message.MESSAGE_TYPE_FIELD);
-                    executor.submit(new Runnable() {
-                        @Override
-                        public void run() {
-                            activity.getActor().receive(type, jsonObject, client.getInetAddress());
-                        }
-                    });
-                    Log.d("Receiver", "'Application' field: " + app + " with name " + name);
-                } catch (IOException e) {
-                    System.out.println(e);
-                } catch (JSONException e) {
-                    Log.d("Receiver", "Not understandable Json object");
+    void process(String json, final InetAddress address) {
+        try {
+            if (!Utils.isJsonObject(json)) return;
+            final JSONObject jsonObject = new JSONObject(json);
+            if(!jsonObject.has(Message.APP_FIELD)) return;
+            String app = jsonObject.getString(Message.APP_FIELD);
+            if(!app.equals(Message.APP_VALUE)) return;
+            String name = jsonObject.getString(Message.NAME_FIELD);
+            final String type = jsonObject.getString(Message.MESSAGE_TYPE_FIELD);
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    activity.getActor().receive(type, jsonObject, address);
                 }
-
-            }
-        } catch (IOException e) {
+            });
+        } catch (JSONException e) {
             e.printStackTrace();
         }
+        Log.d("Receiver", "Message processed");
     }
 
-    public void stop() {
-        try {
-            if (serverSocket != null)
-                serverSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    public abstract void stop();
 }
---
+++ app/src/main/java/com/multipong/net/receive/TCPReceiver.java
@@ -0,0 +1,51 @@
+package com.multipong.net.receive;
+
+import android.util.Log;
+
+import com.multipong.activity.MultiplayerGameFormationActivity;
+import com.multipong.net.Utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class TCPReceiver extends Receiver {
+
+    private ServerSocket serverSocket;
+
+    public TCPReceiver(MultiplayerGameFormationActivity activity) {
+        super(activity);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            serverSocket = new ServerSocket(Utils.PORT);
+            while(true) {
+                final Socket client = serverSocket.accept();
+                try {
+                    InputStream input = client.getInputStream();
+                    Scanner scanner = new Scanner(input).useDelimiter("\\A");
+                    String json = scanner.hasNext() ? scanner.next() : "";
+                    process(json, client.getInetAddress());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            Log.d("TCPReceiver", "Socket closed");
+        }
+    }
+
+    public void stop() {
+        try {
+            if (serverSocket != null)
+                serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

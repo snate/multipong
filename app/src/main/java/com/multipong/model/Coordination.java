d07ddd414c5e83d29c96a88c1be12f2d779ed551
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-11 20:37:35 +0100

    Add Coordination singleton actor initial draft

--- app/src/main/java/com/multipong/model/Coordination.java
+++ app/src/main/java/com/multipong/model/Coordination.java
@@ -1,4 +1,38 @@
 package com.multipong.model;
 
-public class Coordination {
+import com.multipong.activity.MultiplayerGameFormationActivity;
+import com.multipong.net.messages.Message;
+import com.multipong.net.send.Sender.AddressedContent;
+
+import org.json.JSONObject;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public enum Coordination implements Actor {
+
+    INSTANCE;
+
+    // TODO: Refactor to give proper type to activity
+    private MultiplayerGameFormationActivity activity;
+    private static final String WIFI_P2P_GROUP_OWNER_ADDRESS = "192.168.49.1";
+
+    @Override
+    public synchronized void receive(String type, JSONObject message, InetAddress sender) {
+        // TODO: Add implementation
+    }
+
+    public void setActivity(MultiplayerGameFormationActivity activity) {
+        this.activity = activity;
+    }
+
+    public void sendToCoordinator(Message message) {
+        try {
+            InetAddress address = InetAddress.getByName(WIFI_P2P_GROUP_OWNER_ADDRESS);
+            AddressedContent content = new AddressedContent(message, address);
+            activity.addMessageToQueue(content);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
 }

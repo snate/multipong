bd0a9b8dca6cbcd686b694e54a2b2751c38b339c
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-12 11:44:48 +0100

    (Coordination) Forward ball info to active players

--- app/src/main/java/com/multipong/model/Coordination.java
+++ app/src/main/java/com/multipong/model/Coordination.java
@@ -1,15 +1,16 @@
 package com.multipong.model;
 
-import com.multipong.activity.MultiplayerGameFormationActivity;
 import com.multipong.activity.NetworkingActivity;
 import com.multipong.model.multiplayer.MultiplayerStateManager;
-import com.multipong.net.messages.Message;
+import com.multipong.net.NameResolutor;
 import com.multipong.net.messages.game.BallInfoMessage;
 import com.multipong.net.send.Sender.AddressedContent;
+import com.multipong.utility.DeviceIdUtility;
 
 import org.json.JSONObject;
 
 import java.net.InetAddress;
+import java.util.Collection;
 
 public class Coordination implements Actor {
 
@@ -27,9 +28,18 @@ public class Coordination implements Actor {
     private void spreadToParticipants(JSONObject json, InetAddress sender) {
         BallInfoMessage message = BallInfoMessage.createFromJson(json);
         if (message.isForCoordinator()) {
-            // TODO: Send message to peers
-            // TODO: Avoid sending ball info to sender too (not necessary)
-            // TODO: Avoid sending ball info to local MSM
+            message = message.forCoordinator(false);
+            MultiplayerStateManager msm = (MultiplayerStateManager) activity.getActor();
+            // Send message to participants
+            Collection<Integer> ids = msm.getActivePlayers();
+            for (Integer id : ids) {
+                InetAddress address = NameResolutor.INSTANCE.getNodeByHash(id);
+                AddressedContent content = new AddressedContent(message, address);
+                // Avoid sending ball info to local MSM
+                // && Avoid sending ball info to sender too (not necessary)
+                if (!id.equals(DeviceIdUtility.getId()) && !(address.equals(sender))
+                    activity.addMessageToQueue(content);
+            }
             // Forward message to local MSM
             Actor actor = activity.getActor();
             actor.receive(MultiplayerStateManager.MessageType.BALL_INFO, json, sender);

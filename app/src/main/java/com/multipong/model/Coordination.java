e4bf482815e08a4ad8f05ea75c0f26aa694f8504
Sebastiano <sebastiano.valle@studenti.unipd.it>
2017-02-12 11:09:49 +0100

    (Coordination) Forward message to current node if recipient is not coordinator

--- app/src/main/java/com/multipong/model/Coordination.java
+++ app/src/main/java/com/multipong/model/Coordination.java
@@ -1,6 +1,7 @@
 package com.multipong.model;
 
 import com.multipong.activity.MultiplayerGameFormationActivity;
+import com.multipong.activity.NetworkingActivity;
 import com.multipong.model.multiplayer.MultiplayerStateManager;
 import com.multipong.net.messages.Message;
 import com.multipong.net.messages.game.BallInfoMessage;
@@ -13,7 +14,7 @@ import java.net.InetAddress;
 public class Coordination implements Actor {
 
     // TODO: Refactor to give proper type to activity
-    private MultiplayerGameFormationActivity activity;
+    private NetworkingActivity activity;
 
     @Override
     public synchronized void receive(String type, JSONObject message, InetAddress sender) {
@@ -31,11 +32,12 @@ public class Coordination implements Actor {
             // TODO: Avoid sending ball info to sender too (not necessary)
             // TODO: Avoid sending ball info to coordinator node (but deliver the message to MSM!)
         } else {
-            // TODO: Forward message to current node's MSM
+            Actor actor = activity.getActor();
+            actor.receive(MultiplayerStateManager.MessageType.BALL_INFO, json, sender);
         }
     }
 
-    public void setActivity(MultiplayerGameFormationActivity activity) {
+    public void setActivity(NetworkingActivity activity) {
         this.activity = activity;
     }
 }

5d6aa9ecf329f592c61115cbfc3d5e132b1eb374
snate <sebastiano.valle@studenti.unipd.it>
2017-02-14 15:37:45 +0100

    Isolate pinging in an asyncTask

--- app/src/main/java/com/multipong/model/coordination/Pinger.java
+++ app/src/main/java/com/multipong/model/coordination/Pinger.java
@@ -1,11 +1,18 @@
 package com.multipong.model.coordination;
 
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+
 import com.multipong.activity.GameActivity;
+import com.multipong.activity.MainActivity;
+import com.multipong.model.formation.Host;
 import com.multipong.model.game.MultiplayerGame;
 import com.multipong.model.multiplayer.MultiplayerStateManager;
 import com.multipong.model.multiplayer.MultiplayerStateManager.Player;
 import com.multipong.net.messages.game.AreYouAliveMessage;
 import com.multipong.net.send.AckUDPSender;
+import com.multipong.utility.PlayerNameUtility;
 
 import java.net.InetAddress;
 import java.util.ArrayList;
@@ -26,26 +33,45 @@ public abstract class Pinger extends TimerTask {
     public void run() {
         MultiplayerGame multiplayerGame = (MultiplayerGame) networkingActivity.getGame();
         msm = multiplayerGame.getMSM();
-        int attempts = getAttempts();
-        boolean pong = false;
-        while (!pong && attempts < getAttempts()) {
-            savePlayers(msm.getActivePlayers());
-            AreYouAliveMessage ayaMessage = new AreYouAliveMessage();
-            InetAddress address = getPinged();
-            AckUDPSender.ReliablyDeliverableAddressedContent rdac =
-                    new AckUDPSender.ReliablyDeliverableAddressedContent(ayaMessage, address);
-            networkingActivity.addMessageToQueue(rdac);
-            synchronized (rdac) {
-                try {
-                    rdac.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        new AsyncTask<Void, Void, Void>() {
+
+            private boolean pong;
+            private int attempts;
+
+            @Override
+            protected void onPreExecute() {
+                pong = false;
+                attempts = getAttempts();
             }
-            pong = rdac.getB();
-        }
-        if (!pong)
-            pingedIsNotAlive();
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                while (!pong && attempts < getAttempts()) {
+                    savePlayers(msm.getActivePlayers());
+                    AreYouAliveMessage ayaMessage = new AreYouAliveMessage();
+                    InetAddress address = getPinged();
+                    AckUDPSender.ReliablyDeliverableAddressedContent rdac =
+                            new AckUDPSender.ReliablyDeliverableAddressedContent(ayaMessage, address);
+                    networkingActivity.addMessageToQueue(rdac);
+                    synchronized (rdac) {
+                        try {
+                            rdac.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    pong = rdac.getB();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                if (!pong)
+                    pingedIsNotAlive();
+            }
+
+        }.execute(null, null, null);
     }
 
     private void savePlayers(Collection<Integer> activePlayers) {

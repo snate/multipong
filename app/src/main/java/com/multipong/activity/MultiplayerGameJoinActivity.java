90473990b21ba37c73c555a8cd8378cf51b163c1
mzanella <zanna0150@gmail.com>
2017-02-16 14:40:08 +0100

    Modifiche

--- app/src/main/java/com/multipong/activity/MultiplayerGameJoinActivity.java
+++ app/src/main/java/com/multipong/activity/MultiplayerGameJoinActivity.java
@@ -3,6 +3,7 @@ package com.multipong.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

99c1c79f3af9cfbb3d517740badc6e2673085e17
mzanella <zanna0150@gmail.com>
2017-03-15 10:01:12 +0100

    Added log of the wifi direct passphrase

--- app/src/main/java/com/multipong/activity/MultiplayerGameFormationActivity.java
+++ app/src/main/java/com/multipong/activity/MultiplayerGameFormationActivity.java
@@ -1,9 +1,14 @@
 package com.multipong.activity;
 
 import android.content.Context;
+import android.net.wifi.p2p.WifiP2pGroup;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.multipong.R;
 import com.multipong.net.Utils;
 import com.multipong.net.WifiP2pListener;
 import com.multipong.net.messages.PoisonPillMessage;
@@ -34,9 +39,22 @@ public abstract class MultiplayerGameFormationActivity extends NetworkingActivit
     protected void onResume() {
         super.onResume();
 
-        WifiP2pManager manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        final WifiP2pManager manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         WifiP2pManager.Channel channel = manager.initialize(this, getMainLooper(), null);
         listener = new WifiP2pListener(manager, channel, this);
+
+        manager.requestGroupInfo(channel, new WifiP2pManager.GroupInfoListener() {
+            @Override
+            public void onGroupInfoAvailable(WifiP2pGroup group) {
+                if (group.isGroupOwner()) {
+                    String passphrase = group.getPassphrase();
+                    ((TextView)findViewById(R.id.forPassphrase)).setText("PASSPHRASE: " + passphrase);
+                    Toast.makeText(MultiplayerGameFormationActivity.this, "PASSPHRASE: " + passphrase,
+                            Toast.LENGTH_LONG).show();
+                    Log.i("PASSPHRASE", passphrase);
+                }
+            }
+        });
     }
 
     @Override
